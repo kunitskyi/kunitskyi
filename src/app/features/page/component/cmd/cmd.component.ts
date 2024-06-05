37b127505106ba3675eef28cbcb1d4a46e1caed8
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-06-05 09:32:16 -0400

    add: cmd component

---
+++ src/app/features/page/component/cmd/cmd.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kun-cmd',
+  standalone: true,
+  imports: [],
+  templateUrl: './cmd.component.html',
+  styleUrl: './cmd.component.scss',
+})
+export class CmdComponent {}
