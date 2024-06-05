f34aafe4d205877deecfc19eb3365753420eacdd
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-06-05 09:32:36 -0400

    add: cover component

---
+++ src/app/features/page/component/cover/cover.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kun-cover',
+  standalone: true,
+  imports: [],
+  templateUrl: './cover.component.html',
+  styleUrl: './cover.component.scss'
+})
+export class CoverComponent {
+
+}
