d659c1bd09a1ec74a068ea2d36f1f9421408aee3
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-06-05 09:16:03 -0400

    add: index component

---
+++ src/app/features/page/component/index/index.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kun-index',
+  standalone: true,
+  imports: [],
+  templateUrl: './index.component.html',
+  styleUrl: './index.component.scss',
+})
+export class IndexComponent {}
