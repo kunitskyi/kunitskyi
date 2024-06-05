853bd08720946af7b312c6775ebd9b00e3029df1
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-06-05 09:22:26 -0400

    em: default angular template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 /* eslint-disable @angular-eslint/component-selector */
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IndexComponent } from './features/page/component/index/index.component';
 
 @Component({
   selector: 'kunitskyi',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, IndexComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
