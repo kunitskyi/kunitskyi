d99e6cbcd58228ce025b5038a07b59bbc92fa2e4
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-06-11 04:23:36 -0400

    edit: changeView logic in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { IndexComponent } from './features/page/component/index/index.component';
+import { EditorComponent } from './features/code/component/editor/editor.component';
+import { FeatureView } from './types/general-enums';
 
 @Component({
   selector: 'kunitskyi',
   standalone: true,
-  imports: [RouterOutlet, IndexComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  imports: [RouterOutlet, IndexComponent, EditorComponent],
 })
 export class AppComponent {
-  title = 'kunitskyi';
+  protected get FeatureView() {
+    return FeatureView;
+  }
+  protected view: FeatureView = FeatureView.Page;
+
+  protected changeView(e: FeatureView) {
+    this.view = e;
+  }
 }
