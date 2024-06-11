267582f69659e294039364aea0ada201c50447ab
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-06-11 04:25:07 -0400

    edit: IndexComponent to pass from child to parent FeatureView enum value

--- src/app/features/page/component/index/index.component.ts
+++ src/app/features/page/component/index/index.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { CoverComponent } from '@app/features';
+import { FeatureView } from '@app/types/general-enums';
 
 @Component({
   selector: 'kun-index',
   standalone: true,
-  imports: [],
+  imports: [CoverComponent],
   templateUrl: './index.component.html',
   styleUrl: './index.component.scss',
 })
-export class IndexComponent {}
+export class IndexComponent {
+  @Output() changeViewEvent = new EventEmitter<FeatureView>();
+  changeView(e: FeatureView) {
+    this.changeViewEvent.emit(e);
+  }
+}
