8949f6b3a4d5c97a077b7fa0bd427cce6e253211
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-06-13 15:42:37 -0400

    add: transition animation & edit: refactor code in DogEarComponent

--- src/app/component/dog-ear/dog-ear.component.ts
+++ src/app/component/dog-ear/dog-ear.component.ts
@@ -3,6 +3,7 @@ import {
   Component,
   ElementRef,
   EventEmitter,
+  HostBinding,
   Input,
   OnDestroy,
   Output,
@@ -11,7 +12,7 @@ import {
   ViewChild,
 } from '@angular/core';
 import { FeatureView } from '@app/types/general-enums';
-import { Observable, Subscription, fromEvent } from 'rxjs';
+import { Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'kun-dog-ear',
@@ -21,15 +22,29 @@ import { Observable, Subscription, fromEvent } from 'rxjs';
   styleUrl: './dog-ear.component.scss',
 })
 export class DogEarComponent implements AfterViewInit, OnDestroy {
+  @HostBinding('class.Page') get isPageView(): boolean {
+    return this.featureView === FeatureView.Page;
+  }
+  @HostBinding('class.Code') get isCodeView(): boolean {
+    return this.featureView === FeatureView.Code;
+  }
+
   @Output() private startChangeViewEvent = new EventEmitter<boolean>();
   @Output() private changeViewEvent = new EventEmitter<FeatureView>();
 
-  @ViewChild('ear') private earRef!: ElementRef;
+  @ViewChild('earBackground') private earBackgroundRef!: ElementRef;
+  @ViewChild('earTip') private earTipRef!: ElementRef;
+  @ViewChild('content') private contentRef!: ElementRef;
 
   @Input({ required: true }) public featureView!: FeatureView;
   @Input({ required: true }) public isViewChangeTriggered = false;
 
-  private hoverObservable!: Observable<MouseEvent>;
+  private iconSize!: number;
+  private earDivisionLine!: {
+    x: number;
+    y: number;
+  };
+  protected hoverObservable!: Subject<MouseEvent>;
   private hoverSubscription!: Subscription;
   protected get FeatureView() {
     return FeatureView;
@@ -38,46 +53,51 @@ export class DogEarComponent implements AfterViewInit, OnDestroy {
   constructor(
     private renderer: Renderer2,
     private dogEarRef: ElementRef,
-  ) {}
+  ) {
+    this.iconSize = Number(
+      getComputedStyle(dogEarRef.nativeElement).getPropertyValue('--icon-size'),
+    );
+    this.earDivisionLine = {
+      x: this.iconSize,
+      y: this.iconSize,
+    };
+    this.hoverObservable = new Subject<MouseEvent>();
+  }
 
   ngAfterViewInit(): void {
-    this.hoverObservable = fromEvent<MouseEvent>(
-      this.earRef.nativeElement,
-      'mousemove',
-    );
-
     this.hoverSubscription = this.hoverObservable.subscribe((e) => {
-      const relationship: number = e.layerX / e.layerY;
+      this.earDivisionLine.x = e.layerX;
+      this.earDivisionLine.y = e.layerY;
 
-      let cordX: number = e.layerX;
-      let cordY: number = e.layerY;
+      const relationship: number =
+        this.earDivisionLine.x / this.earDivisionLine.y;
 
       if (relationship > 2) {
-        cordX = e.layerX;
-        cordY = e.layerX / 2;
+        this.earDivisionLine.y = this.earDivisionLine.x / 2;
       } else if (relationship < 0.5) {
-        cordX = e.layerY / 2;
-        cordY = e.layerY;
+        this.earDivisionLine.x = this.earDivisionLine.y / 2;
       }
 
       this.renderer.setStyle(
         this.dogEarRef.nativeElement,
-        '--background-computed-path',
+        '--earBackground-computed-path',
         `polygon(
-          ${cordX}px 0,
           0 0,
-          0 ${cordY}px
+          ${this.earDivisionLine.x}px 0,
+          0 ${this.earDivisionLine.y}px,
+          0 ${this.earDivisionLine.y}px
         )`,
         RendererStyleFlags2.DashCase,
       );
 
       this.renderer.setStyle(
         this.dogEarRef.nativeElement,
-        '--ear-computed-path',
+        '--earTip-computed-path',
         `polygon(
-          ${cordX}px 0,
-          0 ${cordY}px,
-          ${e.layerX}px ${e.layerY}px
+          ${this.earDivisionLine.x}px 0,
+          ${e.layerX}px ${e.layerY}px,
+          0 ${this.earDivisionLine.y}px,
+          0 ${this.earDivisionLine.y}px
         )`,
         RendererStyleFlags2.DashCase,
       );
@@ -86,11 +106,11 @@ export class DogEarComponent implements AfterViewInit, OnDestroy {
         this.dogEarRef.nativeElement,
         '--content-computed-path',
         `polygon(
-          0 ${cordY}px,
-          ${cordX}px 0,
+          ${this.earDivisionLine.x}px 0,
           100% 0,
           100% 100%,
-          0 100%
+          0 100%,
+          0 ${this.earDivisionLine.y}px
         )`,
         RendererStyleFlags2.DashCase,
       );
@@ -101,19 +121,124 @@ export class DogEarComponent implements AfterViewInit, OnDestroy {
     this.hoverSubscription.unsubscribe();
   }
 
-  startViewChange(): void {
+  startViewChange(e: MouseEvent): void {
+    const timeInMS = 1000;
+
+    const anglePointPosition =
+      e.layerX === 0 && e.layerY === 0 // in case if button pressed from keyboard
+        ? { x: this.iconSize, y: this.iconSize }
+        : { x: e.layerX, y: e.layerY };
+
     this.isViewChangeTriggered = true;
     this.startChangeViewEvent.emit(this.isViewChangeTriggered);
-    setTimeout(() => {
-      this.changeView();
-    }, 1000);
-  }
 
-  changeView(): void {
-    this.changeViewEvent.emit(
-      this.featureView !== FeatureView.Page
-        ? FeatureView.Page
-        : FeatureView.Code,
+    this.earBackgroundRef.nativeElement.animate(
+      [
+        {
+          opacity: 1,
+          clipPath: `polygon(
+            0 0,
+            ${this.earDivisionLine.x}px 0,
+            0 ${this.earDivisionLine.y}px,
+            0 ${this.earDivisionLine.y}px
+          )`,
+        },
+        {
+          clipPath: `polygon(
+            0 0,
+            10% 0,
+            0 100%,
+            0 100%
+          )`,
+        },
+        {
+          opacity: 0,
+          clipPath: `polygon(
+            0 0,
+            110% 0,
+            100% 100%,
+            0 100%
+          )`,
+        },
+      ],
+      {
+        duration: timeInMS,
+      },
     );
+
+    this.earTipRef.nativeElement.animate(
+      [
+        {
+          clipPath: `polygon(
+            0 ${this.earDivisionLine.y}px,
+            ${this.earDivisionLine.x}px 0,
+            ${anglePointPosition.x}px ${anglePointPosition.y}px,
+            0 ${this.earDivisionLine.y}px
+          )`,
+        },
+        {
+          clipPath: `polygon(
+            0 100%,
+            10% 0,
+            30% 0,
+            0 100%
+          )`,
+        },
+        {
+          clipPath: `polygon(
+            100% 100%,
+            110% 0,
+            130% 0,
+            120% 100%
+          )`,
+        },
+      ],
+      {
+        duration: timeInMS,
+      },
+    );
+
+    this.contentRef.nativeElement.animate(
+      [
+        {
+          clipPath: `polygon(
+            ${this.earDivisionLine.x}px 0,
+            100% 0,
+            100% 100%,
+            0 100%,
+            0 ${this.earDivisionLine.y}px
+          )`,
+        },
+        {
+          clipPath: `polygon(
+            10% 0,
+            100% 0,
+            100% 100%,
+            0 100%,
+            0 100%
+          )`,
+        },
+        {
+          clipPath: `polygon(
+            110% 0,
+            100% 0,
+            100% 100%,
+            100% 100%,
+            100% 100%
+          )`,
+        },
+      ],
+      {
+        duration: timeInMS,
+      },
+    );
+
+    setTimeout(() => {
+      this.changeViewEvent.emit(
+        this.featureView !== FeatureView.Page
+          ? FeatureView.Page
+          : FeatureView.Code,
+      );
+    }, timeInMS);
   }
 }
