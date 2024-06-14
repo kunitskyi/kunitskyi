9a922343d135e33044c4191dff559632c6ab6f8a
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-06-14 15:22:50 -0400

    add: StateBarComponent

---
+++ src/app/features/code/component/state-bar/state-bar.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { StateBarComponent } from './state-bar.component';
+
+describe('StateBarComponent', () => {
+  let component: StateBarComponent;
+  let fixture: ComponentFixture<StateBarComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [StateBarComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(StateBarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/features/code/component/state-bar/state-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kun-state-bar',
+  standalone: true,
+  imports: [],
+  templateUrl: './state-bar.component.html',
+  styleUrl: './state-bar.component.scss',
+})
+export class StateBarComponent {}
