31471342655939888db4dabcc4254039f2740df5
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-06-14 15:24:46 -0400

    add: new components in index file and CodeIndexComponent

--- src/app/features/code/component/code-index/code-index.component.ts
+++ src/app/features/code/component/code-index/code-index.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import {
+  EditorComponent,
+  SidePanelComponent,
+  SidebarComponent,
+  StateBarComponent,
+  WorkbenchComponent,
+} from '@app-code/component';
 
 @Component({
   selector: 'kun-code-index',
   standalone: true,
-  imports: [],
   templateUrl: './code-index.component.html',
   styleUrl: './code-index.component.scss',
+  imports: [
+    SidebarComponent,
+    SidePanelComponent,
+    EditorComponent,
+    WorkbenchComponent,
+    StateBarComponent,
+  ],
 })
 export class CodeIndexComponent {}
---
+++ src/app/features/code/component/index.ts
@@ -0,0 +1,5 @@
+export * from './editor/editor.component';
+export * from './sidebar/sidebar.component';
+export * from './side-panel/side-panel.component';
+export * from './state-bar/state-bar.component';
+export * from './workbench/workbench.component';
