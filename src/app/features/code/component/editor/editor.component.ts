8a4efd98785aedd7bae76aae938fd5765e0741e6
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-06-14 15:21:27 -0400

    add: EditorComponent

---
+++ src/app/features/code/component/editor/editor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kun-editor',
+  standalone: true,
+  imports: [],
+  templateUrl: './editor.component.html',
+  styleUrl: './editor.component.scss',
+})
+export class EditorComponent {}
