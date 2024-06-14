1c3494242991ba83929e70d5e355d6c261fbc9db
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-06-14 15:23:21 -0400

    add: WorkbenchComponent

---
+++ src/app/features/code/component/workbench/workbench.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kun-workbench',
+  standalone: true,
+  imports: [],
+  templateUrl: './workbench.component.html',
+  styleUrl: './workbench.component.scss',
+})
+export class WorkbenchComponent {}
