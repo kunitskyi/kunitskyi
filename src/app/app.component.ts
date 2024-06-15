80641f3a17c48e72885f40898178f99daf6f97c7
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-06-15 16:12:36 -0400

    edit: make title translatable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 /* eslint-disable @angular-eslint/component-selector */
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FeatureView } from '@app/types';
 import { DogEarComponent } from '@app/component';
 import { CodeIndexComponent } from './features/code/component/code-index/code-index.component';
 import { PageIndexComponent } from './features/page/component/page-index/page-index.component';
+import { Title } from '@angular/platform-browser';
+import { TranslocoService } from '@jsverse/transloco';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'kunitskyi',
@@ -18,11 +21,27 @@ import { PageIndexComponent } from './features/page/component/page-index/page-in
     CodeIndexComponent,
   ],
 })
-export class AppComponent {
-  protected view: FeatureView = FeatureView.Page;
+export class AppComponent implements OnInit, OnDestroy {
+  private titleSubscriber!: Subscription;
+  protected view: FeatureView = FeatureView.Code;
   protected isViewChangeTriggered = false;
   protected get FeatureView() {
     return FeatureView;
+  }
+
+  constructor(
+    private translocoService: TranslocoService,
+    private titleService: Title,
+  ) {}
+
+  ngOnInit(): void {
+    this.titleSubscriber = this.translocoService
+      .selectTranslate('author')
+      .subscribe((value) => this.titleService.setTitle(value));
+  }
+
+  ngOnDestroy(): void {
+    this.titleSubscriber.unsubscribe();
   }
 
   protected startChangeView(e: boolean) {
