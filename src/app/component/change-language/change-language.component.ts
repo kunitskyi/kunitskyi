83bca0b3c6651ad1d533b2e64f17457c8f3920bf
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-06-15 16:13:41 -0400

    add: change language logic in ChangeLanguageComponent

--- src/app/component/change-language/change-language.component.ts
+++ src/app/component/change-language/change-language.component.ts
@@ -1,10 +1,11 @@
 import { Component, HostBinding, Input } from '@angular/core';
 import { FeatureView } from '@app/types';
+import { TranslocoPipe, TranslocoService } from '@jsverse/transloco';
 
 @Component({
   selector: 'kun-change-language',
   standalone: true,
-  imports: [],
+  imports: [TranslocoPipe],
   templateUrl: './change-language.component.html',
   styleUrl: './change-language.component.scss',
 })
@@ -12,6 +13,7 @@ export class ChangeLanguageComponent {
   @HostBinding('class.Page') get isPageView(): boolean {
     return this.featureView === FeatureView.Page;
   }
+
   @HostBinding('class.Code') get isCodeView(): boolean {
     return this.featureView === FeatureView.Code;
   }
@@ -19,5 +21,13 @@ export class ChangeLanguageComponent {
   @Input({ required: true }) featureView!: FeatureView;
   protected get FeatureView() {
     return FeatureView;
+  }
+
+  constructor(private translocoService: TranslocoService) {}
+
+  protected changeLanguage(): void {
+    this.translocoService.setActiveLang(
+      this.translocoService.getActiveLang() === 'en' ? 'ua' : 'en',
+    );
   }
 }
