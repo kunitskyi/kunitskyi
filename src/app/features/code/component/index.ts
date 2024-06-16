da82cec8da75c9af467d2a5998b7abd9d85a8051
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-06-16 05:41:57 -0400

    add: styles and html template in NotificationsComponent

--- src/app/features/code/component/index.ts
+++ src/app/features/code/component/index.ts
@@ -2,4 +2,5 @@ export * from './editor/editor.component';
 export * from './sidebar/sidebar.component';
 export * from './side-panel/side-panel.component';
 export * from './state-bar/state-bar.component';
+export * from './notifications/notifications.component';
 export * from './workbench/workbench.component';
--- src/app/features/code/component/notifications/notifications.component.ts
+++ src/app/features/code/component/notifications/notifications.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslocoPipe } from '@jsverse/transloco';
 
 @Component({
   selector: 'kun-notifications',
   standalone: true,
-  imports: [],
+  imports: [TranslocoPipe],
   templateUrl: './notifications.component.html',
   styleUrl: './notifications.component.scss',
 })
