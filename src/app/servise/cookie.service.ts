a8f0bc552be3f675cddf577a106533b38a4f3407
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-06-16 15:53:53 -0400

    add: CookieService logic

--- src/app/servise/cookie.service.ts
+++ src/app/servise/cookie.service.ts
@@ -4,5 +4,26 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class CookieService {
-  constructor() {}
+  public getCookie(name: string): string | undefined | void {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop()?.split(';').shift();
+  }
+
+  public setCookie(
+    name: string,
+    value: string,
+    expires = 24 * 60 * 60,
+    path = '/',
+    domain = '',
+    secure = false,
+    samesite: 'none' | 'lax' | 'strict' = 'strict',
+  ) {
+    const domainAttribute = domain === `` ? `` : `domain=${domain};`;
+    const expiresCompute = new Date(Date.now() + expires);
+    const expiresAttribute = expiresCompute.toUTCString();
+    const SecureAttribute = secure ? 'secure;' : '';
+    document.cookie = `${encodeURIComponent(name)}=${encodeURIComponent(value)}; path=${path}; ${domainAttribute} expires=${expiresAttribute}; ${SecureAttribute} samesite=${samesite}`;
+    return name;
+  }
 }
