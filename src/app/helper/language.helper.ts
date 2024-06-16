1be3b721eda4affcbb5d4523cc9d548eb3d64862
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-06-16 16:56:48 -0400

    add: LanguageHelper

---
+++ src/app/helper/language.helper.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { CookieService } from '@app/servise';
+import { TranslocoService, getBrowserLang } from '@jsverse/transloco';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LanguageHelper {
+  constructor(
+    private translocoService: TranslocoService,
+    private cookieService: CookieService,
+  ) {}
+
+  public setLanguage(lang: string) {
+    this.translocoService.setActiveLang(lang);
+    this.cookieService.setCookie('lang', lang, 14 * 24 * 60 * 60);
+  }
+
+  public initLanguage() {
+    if (this.cookieService.getCookie('lang') === 'ua') this.setLanguage('ua');
+    else if (this.cookieService.getCookie('lang') === 'en')
+      this.setLanguage('en');
+    else if (getBrowserLang() === 'ua') this.setLanguage('ua');
+    else this.setLanguage('en');
+  }
+}
