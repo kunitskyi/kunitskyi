de91d7245c73107030f2769e97211b2d7a89fd93
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-06-29 07:38:51 -0400

    add: change-language in page cover component

--- src/app/features/page/component/cover/cover.component.ts
+++ src/app/features/page/component/cover/cover.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { ChangeLanguageComponent } from '@app/component';
+import { FeatureView } from '@app/types';
 
 @Component({
   selector: 'kun-cover',
   standalone: true,
-  imports: [],
+  imports: [ChangeLanguageComponent],
   templateUrl: './cover.component.html',
   styleUrl: './cover.component.scss',
 })
-export class CoverComponent {}
+export class CoverComponent {
+  protected pageView: FeatureView = FeatureView.Page;
+}
