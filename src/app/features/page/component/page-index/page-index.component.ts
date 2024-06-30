a47d6bb5f4409779f723d46a2d589a9e1d07aba7
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-06-30 13:36:15 -0400

    edit: PageIndexComponent

--- src/app/features/page/component/page-index/page-index.component.ts
+++ src/app/features/page/component/page-index/page-index.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { CoverComponent } from '@app-page/component';
+import { TranslocoPipe } from '@jsverse/transloco';
 
 @Component({
   selector: 'kun-page-index',
   standalone: true,
-  imports: [CoverComponent],
+  imports: [CoverComponent, TranslocoPipe],
   templateUrl: './page-index.component.html',
   styleUrl: './page-index.component.scss',
 })
-export class PageIndexComponent {}
+export class PageIndexComponent {
+  protected mailMe(): void {
+    window.location.href = 'mailto:me@kunitskyi.pp.ua';
+  }
+}
