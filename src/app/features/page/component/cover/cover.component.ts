3392ce4c02800b3d05adf3a8224f3134d5018818
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-06-30 11:16:19 -0400

    edit: CoverComponent

--- src/app/features/page/component/cover/cover.component.ts
+++ src/app/features/page/component/cover/cover.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { ChangeLanguageComponent } from '@app/component';
 import { FeatureView } from '@app/types';
+import { TranslocoPipe } from '@jsverse/transloco';
 
 @Component({
   selector: 'kun-cover',
   standalone: true,
-  imports: [ChangeLanguageComponent],
+  imports: [ChangeLanguageComponent, TranslocoPipe],
   templateUrl: './cover.component.html',
   styleUrl: './cover.component.scss',
 })
 export class CoverComponent {
   protected pageView: FeatureView = FeatureView.Page;
+
+  protected age: number = new Date().getFullYear() - 2002;
 }
