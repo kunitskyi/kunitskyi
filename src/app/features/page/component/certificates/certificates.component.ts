5d93f168eea0928100f84db48cefc740cb30ea2b
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-07-01 14:07:39 -0400

    cmd: ng g c features/page/component/certificates

---
+++ src/app/features/page/component/certificates/certificates.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kun-certificates',
+  standalone: true,
+  imports: [],
+  templateUrl: './certificates.component.html',
+  styleUrl: './certificates.component.scss',
+})
+export class CertificatesComponent {}
