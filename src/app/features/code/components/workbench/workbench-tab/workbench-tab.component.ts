c37a85ba6f0d201ffd072e90dc96113d5c61f8a9
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-07-02 16:00:29 -0400

    add: workbench-tab styles template and logic

--- src/app/features/code/components/workbench/workbench-tab/workbench-tab.component.ts
+++ src/app/features/code/components/workbench/workbench-tab/workbench-tab.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { WorkbenchView } from '@app/features/code/types';
 
 @Component({
   selector: 'kun-workbench-tab',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
   templateUrl: './workbench-tab.component.html',
   styleUrl: './workbench-tab.component.scss',
 })
-export class WorkbenchTabComponent {}
+export class WorkbenchTabComponent {
+  @Output() changeTabEvent = new EventEmitter<WorkbenchView>();
+  @Input({ required: true }) public name!: string;
+  @Input({ required: true }) public view!: WorkbenchView;
+  @Input({ required: true }) public isActive!: boolean;
+
+  protected buttonTriggered() {
+    if (!this.isActive) this.changeTabEvent.emit(this.view);
+  }
+}
