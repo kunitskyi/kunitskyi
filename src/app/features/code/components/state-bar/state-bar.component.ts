59d3f6535377d34b3efcf0fbf466077f4422c20d
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-07-02 15:58:03 -0400

    add: ability to toggle workbench on StateBarComponent

--- src/app/features/code/components/state-bar/state-bar.component.ts
+++ src/app/features/code/components/state-bar/state-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { TranslocoPipe } from '@jsverse/transloco';
+import { WorkbenchView } from '@app-code/types';
 
 @Component({
   selector: 'kun-state-bar',
@@ -9,9 +10,17 @@ import { TranslocoPipe } from '@jsverse/transloco';
   styleUrl: './state-bar.component.scss',
 })
 export class StateBarComponent {
+  @Output() private toggleWorkbenchEvent = new EventEmitter<WorkbenchView>();
   @Output() private toggleNotificationEvent = new EventEmitter<MouseEvent>();
 
   protected currentYear = new Date().getFullYear();
+  protected get WorkbenchView() {
+    return WorkbenchView;
+  }
+
+  protected toggleWorkbench(value: WorkbenchView) {
+    this.toggleWorkbenchEvent.emit(value);
+  }
 
   protected toggleNotifications(e: MouseEvent) {
     this.toggleNotificationEvent.emit(e);
