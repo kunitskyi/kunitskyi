2fbde2d6170c76ec08f4cc185b72f779d4f074d3
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-07-02 16:10:32 -0400

    add: logic template and styles in WorkbenchComponent

--- src/app/features/code/components/workbench/workbench.component.ts
+++ src/app/features/code/components/workbench/workbench.component.ts
@@ -1,14 +1,32 @@
 import { Component, Input } from '@angular/core';
 import { ResizeDirective } from '@app/features/code/directives';
 import { WorkbenchTabComponent } from './workbench-tab/workbench-tab.component';
+import { TranslocoPipe } from '@jsverse/transloco';
+import { WorkbenchView } from '@app-code/types';
+import { TerminalComponent } from '@app/components/terminal/terminal.component';
+import { ProblemsComponent } from './problems/problems.component';
 
 @Component({
   selector: 'kun-workbench',
   standalone: true,
-  imports: [WorkbenchTabComponent, ResizeDirective],
+  imports: [
+    WorkbenchTabComponent,
+    ProblemsComponent,
+    TerminalComponent,
+    ResizeDirective,
+    TranslocoPipe,
+  ],
   templateUrl: './workbench.component.html',
   styleUrl: './workbench.component.scss',
 })
 export class WorkbenchComponent {
-  @Input() isShown = true;
+  @Input() public isShown = true;
+  @Input({ required: true }) public currentView!: WorkbenchView;
+  protected get WorkbenchView() {
+    return WorkbenchView;
+  }
+
+  protected changeTab(value: WorkbenchView) {
+    this.currentView = value;
+  }
 }
