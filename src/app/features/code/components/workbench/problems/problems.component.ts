97030b24f1f9371fa96fa49996c4d9b3561d0735
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-07-02 16:06:05 -0400

    cmd: ng g c features/code/components/workbench/problems

---
+++ src/app/features/code/components/workbench/problems/problems.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kun-problems',
+  standalone: true,
+  imports: [],
+  templateUrl: './problems.component.html',
+  styleUrl: './problems.component.scss',
+})
+export class ProblemsComponent {}
