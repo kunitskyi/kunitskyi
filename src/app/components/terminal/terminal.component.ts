f0afda1d6a9a1b398b9568480cc36a4ca8dabe7e
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-07-03 12:13:23 -0400

    add: simple logic, template and style in TerminalComponent

--- src/app/components/terminal/terminal.component.ts
+++ src/app/components/terminal/terminal.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  Input,
+  ViewChild,
+} from '@angular/core';
+import { FeatureView } from '@app/types';
+import { UserLineComponent } from './user-line/user-line.component';
 
 @Component({
   selector: 'kun-terminal',
   standalone: true,
-  imports: [],
   templateUrl: './terminal.component.html',
   styleUrl: './terminal.component.scss',
+  imports: [UserLineComponent],
 })
-export class TerminalComponent {}
+export class TerminalComponent {
+  @HostBinding('class') private get getClasses() {
+    return {
+      Page: this.view === FeatureView.Page,
+      Code: this.view === FeatureView.Code,
+    };
+  }
+  @HostListener('click') focusOnInput() {
+    this.inputRef.nativeElement.focus();
+  }
+
+  @ViewChild('input') inputRef!: ElementRef;
+
+  @Input({ required: true }) view!: FeatureView;
+}
