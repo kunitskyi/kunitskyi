65a34518adbe950a9a1b79dd47952de604f6ffc2
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-07-05 03:58:04 -0400

    cmd: ng g c features/code/components/editor/editor-tab

---
+++ src/app/features/code/components/editor/editor-tab/editor-tab.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kun-editor-tab',
+  standalone: true,
+  imports: [],
+  templateUrl: './editor-tab.component.html',
+  styleUrl: './editor-tab.component.scss',
+})
+export class EditorTabComponent {}
