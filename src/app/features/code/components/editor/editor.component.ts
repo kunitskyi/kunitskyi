10b0be5688cfb3029366fa37f3de053d85882bd5
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-07-06 08:49:37 -0400

    add: closing and activating tab logic in EditorComponent

--- src/app/features/code/components/editor/editor.component.ts
+++ src/app/features/code/components/editor/editor.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { EditorTabComponent } from './editor-tab/editor-tab.component';
+import { AvailableWorkspaceFiles } from '../../types';
 
 @Component({
   selector: 'kun-editor',
   standalone: true,
-  imports: [],
   templateUrl: './editor.component.html',
   styleUrl: './editor.component.scss',
+  imports: [EditorTabComponent],
 })
-export class EditorComponent {}
+export class EditorComponent {
+  @Input() public set addFile(value: AvailableWorkspaceFiles | undefined) {
+    if (value !== undefined) {
+      this.openFiles.add(value);
+      this.activeFile = value;
+    }
+  }
+
+  protected openFiles = new Set<AvailableWorkspaceFiles>();
+  protected activeFile?: AvailableWorkspaceFiles;
+
+  protected makeActiveTab(file: AvailableWorkspaceFiles) {
+    this.activeFile = file;
+  }
+
+  protected closeTab(file: AvailableWorkspaceFiles) {
+    this.openFiles.delete(file);
+
+    if (this.activeFile === file)
+      if (this.openFiles.size > 0) {
+        this.openFiles.forEach((v1) => {
+          this.activeFile = v1;
+        });
+      } else this.activeFile = undefined;
+  }
+}
