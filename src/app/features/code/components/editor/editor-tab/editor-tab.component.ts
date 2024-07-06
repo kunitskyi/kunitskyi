8c6ac7ccfa9927a09c87dba5abedd2135c4c9ad7
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-07-06 08:45:25 -0400

    add: logic, style and template in EditorTabComponent

--- src/app/features/code/components/editor/editor-tab/editor-tab.component.ts
+++ src/app/features/code/components/editor/editor-tab/editor-tab.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  HostBinding,
+  Input,
+  Output,
+} from '@angular/core';
+import { AvailableWorkspaceFiles } from '@app-code/types';
+import { FILES_METADATA } from '@app-code/constants';
 
 @Component({
   selector: 'kun-editor-tab',
@@ -7,4 +15,26 @@ import { Component } from '@angular/core';
   templateUrl: './editor-tab.component.html',
   styleUrl: './editor-tab.component.scss',
 })
-export class EditorTabComponent {}
+export class EditorTabComponent {
+  @Output() makeActiveTabEvent = new EventEmitter<AvailableWorkspaceFiles>();
+  @Output() closeTabEvent = new EventEmitter<AvailableWorkspaceFiles>();
+
+  @HostBinding('class') private get getClasses() {
+    return {
+      Active: this.isActive,
+    };
+  }
+
+  @Input({ required: true }) public file!: AvailableWorkspaceFiles;
+  @Input() isActive = false;
+
+  protected FILES = FILES_METADATA;
+
+  protected makeActiveTab() {
+    this.makeActiveTabEvent.emit(this.file);
+  }
+
+  protected closeTab() {
+    this.closeTabEvent.emit(this.file);
+  }
+}
