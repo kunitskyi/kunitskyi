9eb7c97cc2e466ff24fda24bc8652eec5b7ac317
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-07-07 15:36:13 -0400

    add: FileLineClass type

--- src/app/features/code/types/editor.ts
+++ src/app/features/code/types/editor.ts
@@ -1,5 +1,18 @@
 export type AvailableWorkspaceFiles = 'profile.js' | 'profile.php';
 
+export type FileLineClass =
+  | 'bold'
+  | 'italic'
+  | 'unused'
+  | 'construction'
+  | 'method'
+  | 'constant'
+  | 'property'
+  | 'string'
+  | 'type'
+  | 'argument'
+  | 'comment';
+
 export interface FileMetaData {
   iconSrc: string;
 }
