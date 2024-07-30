6321474001188140d208403ce669727db364a36a
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-07-30 03:54:45 -0400

    add: 2 diia certificates

--- src/app/features/page/constants/certificates/diia.ts
+++ src/app/features/page/constants/certificates/diia.ts
@@ -22,6 +22,8 @@ function add(
   });
 }
 
+add('D0000123894', 'digigram_civ1', 78, 1615032000, []);
 add('D0000886734', 'digigram_civ2', 73, 1686916800, []);
+add('D0000886850', 'icdl', 21, 1686916800, []);
 
 export const DIIA: Certificate[] = [...certificates];
