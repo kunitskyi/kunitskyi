f4f51e386170a34cdbf372b8fbe6f0047819340b
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-07-30 12:48:05 -0400

    edit: certificates view

--- src/app/features/page/components/certificates/certificates.component.ts
+++ src/app/features/page/components/certificates/certificates.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { CERTIFICATES } from '@app-page/constants';
+import { TranslocoPipe } from '@jsverse/transloco';
+import { Certificate } from '@app-page/types';
+import { DatePipe } from '@angular/common';
+import { PrefixPipe } from '@app/pipes';
 
 @Component({
   selector: 'kun-certificates',
   standalone: true,
-  imports: [],
+  imports: [DatePipe, TranslocoPipe, PrefixPipe],
   templateUrl: './certificates.component.html',
   styleUrl: './certificates.component.scss',
 })
-export class CertificatesComponent {}
+export class CertificatesComponent {
+  protected certificates = CERTIFICATES;
+
+  protected selectCertificate(cert: Certificate) {
+    console.error(cert);
+  }
+}
